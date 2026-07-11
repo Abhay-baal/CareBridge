6963fb73c0fff55c4b348628a1cb6eba485e634b
Abhay-baal <formining2421@gmail.com>
2026-07-11 14:10:47 +0530

    feat: create User and Parent mongoose models

---
+++ backend/src/models/Parent.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const parentSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      unique: true,
+    },
+
+    address: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    emergencyContact: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Parent", parentSchema);
