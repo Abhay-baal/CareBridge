fef3d384146fd9b49ccea8c1d3f51aa7f792df32
Abhay-baal <formining2421@gmail.com>
2026-08-26 20:38:02 +0530

    feat(family): connect parent child chat relationships

--- backend/src/controllers/parentChildController.js
+++ backend/src/controllers/parentChildController.js
@@ -1,5 +1,102 @@
 const ParentChild = require("../models/ParentChild");
 const User = require("../models/User");
+const Family = require("../models/Family");
+
+const syncChildIntoFamily = async (parentId, childId) => {
+  let family = await Family.findOne({
+    $or: [
+      { father: childId },
+      { mother: childId },
+      { children: childId },
+    ],
+  });
+
+  /*
+   * If the child already belongs to a family, add the parent
+   * to the first available parent position.
+   */
+  if (family) {
+    const parentString = parentId.toString();
+
+    if (
+      family.father?.toString() !== parentString &&
+      family.mother?.toString() !== parentString
+    ) {
+      if (!family.father) {
+        family.father = parentId;
+      } else if (!family.mother) {
+        family.mother = parentId;
+      }
+    }
+
+    const childString = childId.toString();
+
+    if (
+      !(family.children || []).some(
+        (child) => child.toString() === childString
+      )
+    ) {
+      family.children.push(childId);
+    }
+
+    await family.save();
+    return family;
+  }
+
+  /*
+   * If the child has no Family yet, check whether the parent
+   * already belongs to one and add the child there.
+   */
+  family = await Family.findOne({
+    $or: [
+      { father: parentId },
+      { mother: parentId },
+      { children: parentId },
+    ],
+  });
+
+  if (family) {
+    const childString = childId.toString();
+
+    if (
+      !(family.children || []).some(
+        (child) => child.toString() === childString
+      )
+    ) {
+      family.children.push(childId);
+      await family.save();
+    }
+
+    return family;
+  }
+
+  /*
+   * Neither user has a Family yet.
+   * Create one using this parent + child pair.
+   */
+  const crypto = require("crypto");
+
+  let familyCode;
+  let exists = true;
+
+  while (exists) {
+    familyCode = crypto
+      .randomBytes(4)
+      .toString("hex")
+      .substring(0, 6)
+      .toUpperCase();
+
+    exists = await Family.exists({
+      familyCode,
+    });
+  }
+
+  return Family.create({
+    familyCode,
+    father: parentId,
+    children: [childId],
+  });
+};
 
 const connectParent = async (req, res) => {
   try {
